e910c67bb98ac53a79ee1177c2ec11ae7b932ccc
Larry N <larryn35@gmail.com>
2020-09-12 18:21:04 -0700

    add astronaut detail view when user selects crew member from mission detail view

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -24,35 +24,33 @@ struct MissionView: View {
             ScrollView(.vertical) {
                 VStack {
                     Image(self.mission.image)
-                    .resizable()
-                    .scaledToFit()
+                        .resizable()
+                        .scaledToFit()
                         .frame(maxWidth: geometry.size.width * 0.7)
                         .padding(.top)
                     
                     Text(self.mission.description)
-                    .padding()
+                        .padding()
                     
                     ForEach(self.astronauts, id: \.role) { crewMember in
-                        HStack {
-                            Image(crewMember.astronaut.id)
-                            .resizable()
-                                .frame(width: 83, height: 60)
-                            .clipShape(Capsule())
-                                .overlay(Capsule().stroke(Color.primary, lineWidth: 1))
-                            
-                            VStack(alignment: .leading) {
-                                Text(crewMember.astronaut.name)
-                                    .font(.headline)
-                                Text(crewMember.role)
-                                    .foregroundColor(.secondary)
+                        NavigationLink(destination: AstronautView(astronaut: crewMember.astronaut)) {
+                            HStack {
+                                Image(crewMember.astronaut.id)
+                                    .resizable()
+                                    .frame(width: 83, height: 60)
+                                    .clipShape(Capsule())
+                                    .overlay(Capsule().stroke(Color.primary, lineWidth: 1))
+                                
+                                VStack(alignment: .leading) {
+                                    Text(crewMember.astronaut.name)
+                                        .font(.headline)
+                                    Text(crewMember.role)
+                                        .foregroundColor(.secondary)
+                                }
                             }
-                            
-                            
                         }
-                        
-                        
+                        .buttonStyle(PlainButtonStyle())
                     }
-                    
                     Spacer(minLength: 25)
                 }
             }
@@ -73,16 +71,14 @@ struct MissionView: View {
                 fatalError("Missing \(member)")
             }
         }
-        
         self.astronauts = matches
     }
-    
 }
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
     static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
-
+    
     
     static var previews: some View {
         MissionView(mission: missions[0], astronauts: astronauts)
